994f8e7b672bac6870580438f714cad40acc0df8
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-01-15 17:38:40 +0300

    feat(Repository): add room entity Db configuraitons

---
+++ Backend/App.Repository/Database/EntityConfigurations/RoomConfiguration.cs
@@ -0,0 +1,21 @@
+using App.Repository.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace App.Repository.Database.EntityConfigurations;
+
+public class RoomConfiguration : IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Description).IsRequired().HasMaxLength(150);
+        builder.Property(x => x.BedCount).IsRequired();
+        builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+        builder.Property(x => x.Title).IsRequired().HasMaxLength(30);
+        builder.Property(x => x.BathCount).IsRequired();
+        builder.Property(x => x.CoverImage).IsRequired();
+        builder.Property(x => x.HasWifi).IsRequired();
+        builder.Property(x => x.RoomNumber).HasMaxLength(4);
+    }
+}
