99c2190ea41d57efd995aa2f2a6a5ffc1ee60b14
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-02-08 18:05:40 +0300

    feat(Service): add StaffServices

---
+++ Backend/App.Service/Services/StaffServices/IStaffService.cs
@@ -0,0 +1,17 @@
+using App.Repository.Entities.Concrete;
+using App.Service.Dtos.StaffDtos;
+using App.Service.Dtos.StaffDtos.Requests;
+using App.Service.Dtos.StaffDtos.Responses;
+using App.Service.Services.GenericServices;
+
+namespace App.Service.Services.StaffServices;
+
+public interface IStaffService : IGenericService<
+    CreateStaffRequest,
+    UpdateStaffRequest,
+    StaffDto,
+    CreateStaffResponse,
+    Staff>
+{
+    
+}
---
+++ Backend/App.Service/Services/StaffServices/StaffService.cs
@@ -0,0 +1,23 @@
+using App.Repository.Entities.Concrete;
+using App.Repository.GenericRepositories;
+using App.Repository.UnitOfWork;
+using App.Service.Dtos.StaffDtos;
+using App.Service.Dtos.StaffDtos.Requests;
+using App.Service.Dtos.StaffDtos.Responses;
+using App.Service.Services.GenericServices;
+using AutoMapper;
+
+namespace App.Service.Services.StaffServices;
+
+public class StaffService : GenericService<
+    CreateStaffRequest,
+    UpdateStaffRequest,
+    StaffDto,
+    CreateStaffResponse,
+    Staff>, IStaffService
+{
+    public StaffService(IGenericRepository<Staff> genericRepository, IMapper mapper, IUnitOfWork unitOfWork) 
+        : base(genericRepository, mapper, unitOfWork)
+    {
+    }
+}
